78c47bebb9ed8e90c5ed7d824292daee6b423c33
Hanns Holger Rutz <contact@sciss.de>
2019-08-14 20:50:13 +0200

    ok; now the example just needs to get the scala class path I guess

--- src/main/scala/com/novocode/extradoc/ExtraDocSettings.scala
+++ src/main/scala/com/novocode/extradoc/ExtraDocSettings.scala
@@ -2,35 +2,12 @@ package com.novocode.extradoc
 
 import scala.tools.nsc._
 
-class ExtraDocSettings(error: String => Unit) extends {
+class ExtraDocSettings(error: String => Unit) extends doc.Settings(error) {
 
-  // XXX TODO:
-
-//  override def ChoiceSetting(name: String, descr: String, choices: List[String], default: String) = {
-//    if(name == "-doc-format") super.ChoiceSetting(name, descr, List("html", "json", "json-multi", "explorer"), default)
-//    else super.ChoiceSetting(name, descr, choices, default)
-//  }
-
-  override val docformat: ChoiceSetting = ChoiceSetting (
-    "-doc-format",
-    "format",
-    "Selects in which format documentation is rendered.",
-    List("html", "json", "json-multi", "explorer"),
-    "html"
-  )
-
-//  def enhancedDocFormat: Setting = ChoiceSetting (
-//    "-doc-format",
-//    "format",
-//    "Selects in which format documentation is rendered.",
-//    List("html", "json", "json-multi", "explorer"),
-//    "html"
-//  )
-
-//  // otherwise we run into NPE due to initialization order
-//  override val isScaladocSpecific: String => Boolean = scaladocSpecific map (_.name)
-//
-//  // XXX TODO: how to specify the return type? bloody projection here
-//  override def scaladocSpecific =
-//    super.scaladocSpecific - docformat + enhancedDocFormat
-} with doc.Settings(error)
+  override def ChoiceSetting(name: String, helpArg: String, descr: String, choices: List[String],
+                             default: String, choicesHelp: List[String]): ChoiceSetting = {
+    val choices1 = if (name != "-doc-format") choices else List("html", "json", "json-multi", "explorer")
+    super.ChoiceSetting(name, helpArg = helpArg, descr = descr, choices = choices1,
+      default = default, choicesHelp = choicesHelp)
+  }
+}
